9d551d18fcea8070aa9e0b2a3a7d6ca0f24701ca
anealbening <anealbening@hotmail.co.uk>
2018-05-23 12:20:52 +0100

    Testing the JSON for News
    Status: Currently not working.
    All files created by Aneal so far.

---
+++ EmptySite1/Script.js
@@ -0,0 +1,43 @@
+function createNode(element)
+{
+    return.document.createElement(element);
+}
+function append(parent,el)
+
+{
+    
+    return parent.appendChild(el)
+}
+
+const ul = document.getElementById("news");
+
+const url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=08d25b5b634d405187c93540c61ee6f1"
+
+fetch(url)
+.then((resp) => resp.json())
+.then(function(data){
+    let news = data.results;
+
+    return news.map(function(news){
+
+        let li = createNode('li'),
+        img = createNode('img'),
+        span = createNode('span');
+
+        img.src=news.urlToImage.medium;
+
+        span.innerHTML='${news.name}
+        ${news.name}';
+
+        append(li,img);
+        append(li,span);
+        append(ul,li);
+    })
+})
+
+.catch(function(error))
+
+
+
+}
+)
